c57722cc97eb11f9e69d5b3e827a18a679fade7b
Nicolás Cieri <nico.cieri13@gmail.com>
2025-09-01 17:59:58 -0300

    solución

--- src/filtros/validaciones.js
+++ src/filtros/validaciones.js
@@ -1,1 +1,27 @@
+export const validaciones = [
+  {
+    regla: "stock",
+    fn: ({ stock }, value) => stock > value,
+  },
+  {
+    regla: "precio",
+    fn: ({ precio }, value) => precio <= value,
+  },
 
+  {
+    regla: "nombre",
+    fn: ({ nombre }, value) => nombre.length < value,
+  },
+  {
+    regla: "categoria",
+    fn: ({ categorias }, value) => value.includes(categorias),
+  },
+  {
+    regla: "precioRango",
+    fn: ({ precio }, value) => precio < value.min && precio > value.max,
+  },
+  {
+    regla: "stockRango",
+    fn: ({ stock }, value) => stock < value.min && stock > value.max,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+import express from "express";
+import productos from "../data/productos.json" with { type: "json" };
+import { validaciones } from "./filtros/validaciones.js";
 
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+const validacionesDiccionario = validaciones.reduce((acc, { regla, fn }) => {
+  acc[regla] = fn;
+  return acc;
+}, {});
+
+const cumpleConFiltros = (filtros) => (producto) =>
+  filtros.every(({ fn, values }) =>
+    validacionesDiccionario[fn](producto, values)
+  );
+
+app.get("/productos", (req, res) => {
+  res.json(productos);
+});
+
+app.post("/productos/filtrar", (req, res) => {
+  const filtros = req.body;
+  const productosFiltrados = productos.filter(cumpleConFiltros(filtros));
+  res.json(productosFiltrados);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server iniciado en puerto ${PORT}`);
+});
